4f6ddb4b4bec40c15373c7eea29741779aa6bd77
MhmdMC <mhmd.abraya@gmail.com>
2025-09-28 22:59:12 +0300

    port changed

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def uptime():
     return render_template("uptime.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
 
 
 
